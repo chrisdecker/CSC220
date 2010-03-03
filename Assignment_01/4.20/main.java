1cd9165a395d5c7fa1c630e64ca49e5067548603
Erik Youngren <artanis.00@gmail.com>
2010-03-03 15:16:26 -0800

    Assignment 01 - Begin

---
+++ Assignment_01/4.20/main.java
@@ -0,0 +1,27 @@
+/*
+Author:       Erik Youngren <eyoungre@callutheran.edu>
+Date Created: 2010-03-02
+
+Purpose
+=======
+Solution to Programming Exercise 4.20:
+
+    Printing Prime Numbers Between 2 and 1000.
+    
+    Modify Listing 4,14 to print all the prime numbers between 2 and 1000,
+    inclusively. Display eigth prime numbers per line.
+
+Document History
+================
++---------------+--------------------------------------------------------------+
+| Date Modified | Reason                                                       |
++---------------+--------------------------------------------------------------+
+| 2010-03-02    | Document Created                                             |
+|               |                                                              |
++---------------+--------------------------------------------------------------+
+
+*/
+
+class main (String[] args) {
+    // code
+}
---
+++ Assignment_01/6.24/main.java
@@ -0,0 +1,35 @@
+/*
+Author:       Erik Youngren <eyoungre@callutheran.edu>
+Date Created: 2010-03-02
+
+Purpose
+=======
+Solution to Programming Exercise 4.24:
+
+    Adding two Matrices
+    
+    Write a method to add two matrices. The header of the method is as follows:
+    
+    .. code-block:: java
+        
+        public static in[][] addMatrix(int[][] a, int[][] b)
+    
+    In order to be added, the two matrices must have the same dimensions and the
+    same or compatible types of elements. As shown below, two matrices are added
+    by adding the two elements in each matrix with the same index.
+    
+
+Document History
+================
++---------------+--------------------------------------------------------------+
+| Date Modified | Reason                                                       |
++---------------+--------------------------------------------------------------+
+| 2010-03-02    | Document Created                                             |
+|               |                                                              |
++---------------+--------------------------------------------------------------+
+
+*/
+
+class main (String[] args) {
+    // code
+}
