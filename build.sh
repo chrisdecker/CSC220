4e4a7b7a70317c5da477adfee70099fb048fc4d5
Erik Youngren <artanis.00@gmail.com>
2010-03-05 22:38:16 -0800

    Rearranged the order of appearance for each section.
    Non-programming problems now on first page, and programming exercises are now on their own pages subsequent.

--- build.sh
+++ build.sh
@@ -1,22 +1,23 @@
 #! /bin/bash
 
-basedir=`pwd $0`
-
 # Source and target directories/files
 source="Assignment_$1"
 rst="$source/$source.rst"
 pdf="$source/$source.pdf"
-
-# Compile all exercises
-for jv in `ls $source/*/*.java`; do
-    javac $jv
-done;
 
 # Build header (clobber the file to begin fresh)
 cat .build/header.rst > $rst
 echo "=================================================================" >> $rst
 echo "Assignment $1" >> $rst
 echo "=================================================================" >> $rst
+
+# Append the non-programming exercises
+echo "" >> $rst
+echo "Problems" >> $rst
+echo "========" >> $rst
+cat $source/exercises.rst >> $rst
+
+echo -e "\n.. raw:: pdf\n    \n    PageBreak\n" >> $rst
 
 # Run the programming exercises and append the output
 echo "" >> $rst
@@ -32,15 +33,10 @@ for f in `ls $source/*/OUTPUT`; do
     do
         echo -e "    $line" >> $rst
     done
+    echo -e "\n.. raw:: pdf\n    \n    PageBreak\n" >> $rst
 done;
 
-cd "$basedir"
-
-# Append the non-programming exercises
-echo "" >> $rst
-echo "Exercises" >> $rst
-echo "=========" >> $rst
-cat $source/exercises.rst >> $rst
+echo ""
 
 # Convert to PDF
 rst2pdf $rst -o $pdf
