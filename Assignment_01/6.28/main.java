a26084f657ea8e902149e1159dac83bd36d31a6c
Erik Youngren <artanis.00@gmail.com>
2010-03-05 22:40:02 -0800

    Final programming exercise for Assignment 01 complete.
    **Now** Assignment 01 is complete.

--- Assignment_01/6.28/main.java
+++ Assignment_01/6.28/main.java
@@ -13,12 +13,12 @@ Solution to Programming Exercise 6.28:
     a two dimensional array to represent a TicTacToe boeard. Here is a sample
     run of the program:
     
-    .. code-block::rst
+    ::
         
         001
         001
         111
-        All 1's on ow 2
+        All 1's on row 2
         All 1s on column 2
 
 Document History
@@ -36,6 +36,42 @@ Date Modified Reason
  */
 class main {
     public static void main (String[] args) {
+        int[][] tictactoe = new int[3][3];
+        
+        System.out.print("\n");
+        
+        // populate the board
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                tictactoe[i][j] = (int)Math.round(Math.random());
+                //System.out.printf(" (%d, %d) = %d |", i, j, tictactoe[i][j]);
+                System.out.printf("%d ", tictactoe[i][j]);
+            }
+            System.out.printf("\n");
+        }
+        
+        // check rows
+        for(int i = 0; i < 3; i++) {
+            if(tictactoe[i][0] == tictactoe[i][1] && tictactoe[i][1] == tictactoe[i][2]) {
+                System.out.printf("All %d's on row %d\n", tictactoe[i][0], i);
+            }
+        }
+        
+        // check columns
+        for(int j = 0; j < 3; j++) {
+            if(tictactoe[0][j] == tictactoe[1][j] && tictactoe[1][j] == tictactoe[2][j]) {
+                System.out.printf("All %d's on column %d\n", tictactoe[0][j], j);
+            }
+        }
+        
+        // check diagonals
+        if(tictactoe[0][0] == tictactoe[1][1] && tictactoe[1][1] == tictactoe[2][2]) {
+            System.out.printf("All %d's on back diagonal\n", tictactoe[0][0]);
+        }
+        
+        if(tictactoe[0][2] == tictactoe[1][1] && tictactoe[1][1] == tictactoe[2][0]) {
+            System.out.printf("All %d's on forward diagonal\n", tictactoe[0][2]);
+        }
     }
 }
 
