80cb42faf13bd6d0a42467e56a86305c00f8915f
Erik Youngren <artanis.00@gmail.com>
2010-03-16 20:24:28 -0700

    9.7 Complete

--- Assignment_03/9.7/main.java
+++ Assignment_03/9.7/main.java
@@ -17,7 +17,7 @@ To solve Programming Exercise 9.7
     4    5    6   
     GHI  JKL  MNO 
     7    8    9   
-    PQRS TUV  XXYZ
+    PQRS TUV  WXYZ
          0        
     ==== ==== ====
     
@@ -49,16 +49,56 @@ Date Modified Reason
 
 */
 
+import java.util.Scanner;
+
 /**
  * Driver class
  */
 class main {
+    /** Lookup string for phone alphanumerics to numeric.
+     * 
+     * The trailing space is important.
+     * 
+     */
+    static final String lookup = "ABC2 DEF3 GHI4 JKL5 MNO6 PQRS7 TUV8 WXYZ9 ";
+    
     public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
         
+        System.out.print("Enter a string: ");
+        String telephone = input.nextLine().toUpperCase();
+        String telephone2 = "";
+        
+        for(int i = 0; i < telephone.length(); i++) {
+            int digit = main.getNumber(telephone.charAt(i));
+            
+            // Valid responses are 2 through 9. Any other value is wrong. In
+            // those cases, the original character is preserved in the result.
+            if(digit >= 2 && digit <= 9) {
+                telephone2 += digit;
+            } else {
+                telephone2 += telephone.charAt(i);
+            }
+        }
+        System.out.println(telephone2);
     }
     
     public static int getNumber(char uppercaseLetter) {
+        // ensure the character is valid.
+        if (uppercaseLetter >= 'A' && uppercaseLetter <= 'Z') {
+            // find the index of the space after the character group the given
+            // character is in
+            int index_of_char = main.lookup.indexOf(uppercaseLetter);
+            int index_of_sep  = main.lookup.indexOf(" ", index_of_char);
+            
+            // extract the digit for that group and convert to integer
+            int digit = Integer.parseInt(main.lookup.substring(
+                index_of_sep - 1, index_of_sep));
+            
+            return digit;
+        }
         
+        return -1;
     }
 }
 
