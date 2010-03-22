340ae6540e75fc7b8cab67431ac0eb438d254958
Erik Youngren <artanis.00@gmail.com>
2010-03-22 15:10:02 -0700

    Replace builder script
    Now using a python script that is easier to understand and produces better output.

---
+++ build.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+
+import sys
+from glob import glob
+
+source = "Assignment_%s" % (sys.argv[1],)
+
+print open(".build/header.rst").read(),
+
+print "=" * len(source)
+print source
+print "=" * len(source)
+
+print "\nProblems\n========\n"
+
+print open("%s/exercises.rst" % (source,)).read(),
+
+print "Programming Exercises\n=====================\n"
+
+for exercise in glob("%s/*" % (source,)):
+    if "exercise" in exercise:
+        continue
+    
+    ex_title = exercise[exercise.rfind("/")+1:]
+    
+    print "-" * len(ex_title)
+    print ex_title
+    print "-" * len(ex_title)
+    
+    for code in glob("%s/*" % (exercise,)):
+        if "~" not in code and ".java" in code:
+            print "\nCode\n----\n.. code-block:: java\n    "
+            f = open(code)
+            for line in f:
+                print "    %s" % (line,),
+            f.close()
+    
+    for output in glob("%s/OUTPUT" % (exercise,)):
+        print "\nOutput\n------\n::\n    "
+        f = open(output)
+        for line in f:
+            print "    %s" % (line,),
+        f.close()
+    
+    print ""
+
