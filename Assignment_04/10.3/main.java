4e65e1e8357e9f0c4fda3f1678fa882b8c775d2b
Erik Youngren <artanis.00@gmail.com>
2010-03-23 21:31:30 -0700

    Assignment 04 10.3 complete

--- Assignment_04/10.3/main.java
+++ Assignment_04/10.3/main.java
@@ -29,8 +29,93 @@ To solve the Programming Exercise 10.3 (7e 9.3)
 /**
  * Driver class
  */
-class main {
+public class main {
     public static void main (String[] args) {
+        MyPoint a = new MyPoint(0, 0);
+        MyPoint b = new MyPoint(10, 30.5);
+        
+        System.out.printf(
+            "The distance between the points %s and %s is %f.\n",
+            a, b, b.distance(a));
     }
 }
 
+class MyPoint {
+    double __x;
+    double __y;
+    
+    public MyPoint() {
+        this(0, 0);
+    }
+    
+    public MyPoint(double x, double y) {
+        this.__x = x;
+        this.__y = y;
+    }
+    
+     /**
+     * Calculate the distance between this instance of MyPoint and the
+     * provided coordinates.
+     * 
+     */
+    public double distance(double x, double y) {
+        // d = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+        return Math.sqrt(
+            (x - this.__x)*(x - this.__x) +
+            (y - this.__y)*(y - this.__y));
+    }
+    
+    /**
+     * Calculate the distance between this and another instance of
+     * MyPoint.
+     * 
+     * Calls the other MyPoint instance's distance(double, double)
+     * method with the calling instance's coordinates. 
+     * I.E: `p1.distance(p2)` * is equivalent to
+     * `p2.distance(p1.__x, p1.__y)`.
+     * 
+     */
+    public double distance(MyPoint other) {
+        return other.distance(this.__x, this.__y);
+    }
+    
+    /**
+     * Distance from (0, 0)
+     * 
+     */
+    public double distance() {
+        return this.distance(0.0, 0.0);
+    }
+    
+    /**
+     * Returns a constructor statement for this instance of MyPoint.
+     * 
+     */
+    public String __repr__() {
+        return String.format("MyPoint(%f, %f)", this.__x, this.__y);
+    }
+    
+    /**
+     * Returns a human readable representation of MyPoint.
+     * 
+     */
+    public String toString() {
+        return String.format("(%f, %f)", this.__x, this.__y);
+    }
+    
+    /**
+     * Completely pointlessly fetch the x-coordinate of MyPoint.
+     * 
+     */
+    //public double getX() {
+    //    return this.__x;
+    //}
+    
+    /**
+     * Completely pointlessly fetch the y-coordinate of MyPoint.
+     * 
+     */
+    //public double getY() {
+    //    return this.__y;
+    //}
+}
