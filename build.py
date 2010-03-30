a1dacb46c1f0e0851c6741a727f384dfd3e55428
Erik Youngren <artanis.00@gmail.com>
2010-03-30 13:34:36 -0700

    Better check for programming exercises (explicitly accept only directories, rather than try to filter out other files.)

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import os
 import sys
 from glob import glob
 
@@ -18,30 +19,28 @@ print open("%s/exercises.rst" % (source,)).read(),
 print "Programming Exercises\n=====================\n"
 
 for exercise in glob("%s/*" % (source,)):
-    if "exercise" in exercise:
-        continue
-    
-    ex_title = exercise[exercise.rfind("/")+1:]
-    
-    print "-" * len(ex_title)
-    print ex_title
-    print "-" * len(ex_title)
-    
-    for code in glob("%s/*" % (exercise,)):
-        if "~" not in code and ".java" in code:
-            #print "\nCode\n----"
-            print ".. code-block:: java\n    "
-            f = open(code)
+    if os.path.isdir(exercise):
+        ex_title = exercise[exercise.rfind("/")+1:]
+        
+        print "-" * len(ex_title)
+        print ex_title
+        print "-" * len(ex_title)
+        
+        for code in glob("%s/*" % (exercise,)):
+            if "~" not in code and ".java" in code:
+                #print "\nCode\n----"
+                print ".. code-block:: java\n    "
+                f = open(code)
+                for line in f:
+                    print "    %s" % (line,),
+                f.close()
+        
+        for output in glob("%s/OUTPUT" % (exercise,)):
+            print "\nOutput\n------\n::\n    "
+            f = open(output)
             for line in f:
                 print "    %s" % (line,),
             f.close()
-    
-    for output in glob("%s/OUTPUT" % (exercise,)):
-        print "\nOutput\n------\n::\n    "
-        f = open(output)
-        for line in f:
-            print "    %s" % (line,),
-        f.close()
-    
-    print ""
+        
+        print ""
 
