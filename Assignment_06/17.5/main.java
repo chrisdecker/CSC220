7cd30ae4a0a33e153557f7dfb8241348c3c41bb9
Erik Youngren <artanis.00@gmail.com>
2010-04-10 03:16:07 -0700

    Description must be updated.
    Reverted away the code in previous commit, but the description needs to be for the correct exercise.

--- Assignment_06/17.5/main.java
+++ Assignment_06/17.5/main.java
@@ -4,16 +4,17 @@ Date Created: 2010-00-00
 
 Purpose
 =======
-To solve the Programming Exercise 17.5 (7e 16.5)
+To solve the Programming Exercise 17.5 (7e 16.21)
 
-    Create a mile/kilometer coverter
-    --------------------------------
-    Write a program that converts miles and kilometers as shown in
-    Figure (7e 16.73). If you enter a value in the Mile text field and
-    press the ``Enter`` key, the corresponding kilometer is displayed
-    in the Kilometer text field, Likewise, if you enter a value in the
-    Kilometer text field and press the ``Enter`` key, the corresponding
-    mile is displayed in the Mile text field.
+    Creating a histogram for occurrences of letters
+    -----------------------------------------------
+    In Listing (7e 16.12), MultipleWindowsDemo.java,you developed a
+    program that displays a histogram to show occurrences of each
+    letter in a text area. Reuse the ``Histogram`` class created in
+    Listing (7e 16.12) to write a program that will display a histogram
+    on a panel. The histogram should show occurrences of each letter in
+    a text file, as shown in Figure (7e 16.51). Assume that the letters
+    are not case sensitive.
 
 */
 
