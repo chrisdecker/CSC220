2477c04ab01b79ef7ef20a097e9a9735eade1c89
Erik Youngren <artanis.00@gmail.com>
2010-04-18 13:04:36 -0700

    Some things don't need to be class attributes.

--- Assignment_06/17.17/main.java
+++ Assignment_06/17.17/main.java
@@ -9,7 +9,7 @@ To solve the Programming Exercise 17.17 (7e 16.7)
     Displaying a Calendar
     ---------------------
     Write a program that displays the calendar for the current month,
-    as shown in figure (7e 16.48). Use labels, and set texts ib the
+    as shown in figure (7e 16.48). Use labels, and set texts in the
     labels to display the calendar. Use the Gregorian Calendar class in
     section 11.3, "Example: ``Calendar`` and ``GregorianCalendar``" to
     obtain the information about the month, year, first day of the
@@ -21,6 +21,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.GridLayout;
+
+import java.awt.Color;
+
+import java.text.DateFormatSymbols;
+import java.util.GregorianCalendar;
 
 /**
  * Driver class
@@ -35,28 +40,31 @@ public class main {
 }
 
 class CalendarWindow extends JFrame {
-    JLabel date = new JLabel();
-    JLabel day_names[] = new JLabel[7];
-    JLabel days[] = new JLabel[31];
-    
     CalendarWindow() {
         this.setLayout(new GridLayout(0,1,5,5));
-        this.setSize(400,300);
+        this.setSize(600,480);
         
-        JPanel day_labels = new JPanel(new GridLayout(1,0,5,5));
+        JPanel weekday_labels = new JPanel(new GridLayout(1,0,5,5));
         
-        for(int i = 0; i < this.day_names.length; i++) {
-            this.day_names[i] = new JLabel();
-            day_labels.add(this.day_names[i]);
+        DateFormatSymbols symbols = new DateFormatSymbols();
+        String[] weekday_symbols = symbols.getWeekdays();
+        
+        for(int i = 1; i < weekday_symbols.length; i++) {
+            JLabel weekday = new JLabel(weekday_symbols[i]);
+            weekday.setHorizontalAlignment(JLabel.CENTER);
+            weekday.setVerticalAlignment(JLabel.BOTTOM);
+            weekday_labels.add(weekday);
         }
         
         JPanel day_grid = new JPanel(new GridLayout(0,7,5,5));
         
-        for (int i = 0; i < this.days.length; i++) {
-            this.days[i] = new JLabel();
-            day_grid.add(this.days[i]);
+        for (int i = 0; i < 31; i++) {
+            JLabel day = new JLabel(""+i);
+            day.setHorizontalAlignment(JLabel.CENTER);
+            day_grid.add(day);
         }
         
-        
+        this.add(weekday_labels);
+        this.add(day_grid);
     }
 }
