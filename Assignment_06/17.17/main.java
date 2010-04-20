5af71b80c5c9456e284154510f489d51bf187a92
Erik Youngren <artanis.00@gmail.com>
2010-04-19 23:54:32 -0700

    A6 17.17 complete.
    If I had made commits for each little change I'd have never finished.
    
    Fixed up the layout so the weekday names row took a sane amount of space.
    Offset the first day of the month so that it lands on the right weekday.
    Added an arg and made a new no-arg constructor. Both build
    GregorianCalendar objects for the real constructor.
    Set window title to the date.
    Probably some other stuff.
    
    hate gregorian calendar now. never want to see again.

--- Assignment_06/17.17/main.java
+++ Assignment_06/17.17/main.java
@@ -17,14 +17,17 @@ To solve the Programming Exercise 17.17 (7e 16.7)
 
 */
 
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-import java.awt.GridLayout;
-
-import java.awt.Color;
 
 import java.text.DateFormatSymbols;
+
 import java.util.GregorianCalendar;
 
 /**
@@ -40,15 +43,48 @@ public class main {
 }
 
 class CalendarWindow extends JFrame {
+    /**
+     * Constructor for current date.
+     * 
+     */
     CalendarWindow() {
-        this.setLayout(new GridLayout(0,1,5,5));
-        this.setSize(600,480);
+        this(new GregorianCalendar());
+    }
+    
+    /**
+     * Constructor for specific date.
+     * 
+     */
+    CalendarWindow(int year, int month, int day) {
+        this(new GregorianCalendar(year, month, day));
+    }
+    
+    /**
+     * Assemble a CalendarWindow object for the month the given
+     * GregorianCalendar object specifies.
+     *
+     */
+    CalendarWindow(GregorianCalendar date) {
+        this.setLayout(new BorderLayout(5,5));
+        this.setSize(300,240);
         
-        JPanel weekday_labels = new JPanel(new GridLayout(1,0,5,5));
-        
+        // retrieve symbols for day and month names
+        // side effect: script is locale ready assuming arabic numerals
         DateFormatSymbols symbols = new DateFormatSymbols();
         String[] weekday_symbols = symbols.getWeekdays();
+        String[] month_symbols = symbols.getMonths();
         
+        // set the title to something useful
+        this.setTitle(String.format(
+            "%s %s %01d, %d",
+            weekday_symbols[date.get(date.DAY_OF_WEEK)],
+            month_symbols[date.get(date.MONTH)],
+            date.get(date.DATE),
+            date.get(date.YEAR)));
+        
+        JPanel weekday_labels = new JPanel(new GridLayout(0,7,5,5));
+        
+        // populate the weekday names row
         for(int i = 1; i < weekday_symbols.length; i++) {
             JLabel weekday = new JLabel(weekday_symbols[i]);
             weekday.setHorizontalAlignment(JLabel.CENTER);
@@ -58,13 +94,46 @@ class CalendarWindow extends JFrame {
         
         JPanel day_grid = new JPanel(new GridLayout(0,7,5,5));
         
-        for (int i = 0; i < 31; i++) {
-            JLabel day = new JLabel(""+i);
-            day.setHorizontalAlignment(JLabel.CENTER);
-            day_grid.add(day);
+        // retrieve the weekday identifier for the first of the month.
+        // why there isn't a function that grabs this vital information
+        // I do not know.
+        int first = new GregorianCalendar(date.get(date.YEAR),
+            date.get(date.MONTH), 1).get(date.DAY_OF_WEEK);
+        
+        // populate the day of month grid
+        // loops from 1 to the number of days in the month, plus an
+        // some for the weekday offset.
+        for (int i = 1; i < date.getActualMaximum(date.DAY_OF_MONTH)+first; i++) {
+            // The ACTUAL maximum because I really want the length of
+            // the month, and not the maximum value the field holds.
+            //
+            // for the future: how about getMaximum(DAY_OF_MONTH) for
+            // length of month and getMaximumValue(DAY_OF_MONTH) for
+            // the maximum value the field supports.
+            
+            // offset the labels so the first fall on the correct
+            // weekday
+            if(i-first+1 > 0) {
+                JLabel day;
+                day = new JLabel(""+(i-first+1));
+                
+                if(i-first+1 == date.get(date.DAY_OF_MONTH)) {
+                    day.setForeground(Color.RED);
+                }
+                
+                day.setHorizontalAlignment(JLabel.CENTER);
+                day_grid.add(day);
+            } else {
+                // placeholder, since you can't skip cells.
+                JPanel empty = new JPanel();
+                day_grid.add(empty);
+            }
         }
         
-        this.add(weekday_labels);
-        this.add(day_grid);
+        // Add everything to the window. All two of them.
+        // weekdays on a border because those collapse, days in center
+        // because it expands.
+        this.add(weekday_labels, BorderLayout.NORTH);
+        this.add(day_grid, BorderLayout.CENTER);
     }
 }
