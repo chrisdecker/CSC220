6c48480a8990f61fffd6fa15fd914e1db1d2edcf
Erik Youngren <artanis.00@gmail.com>
2010-04-30 01:06:32 -0700

    A7 P20.5 complete

--- Assignment_07/20.5/Sigma.java
+++ Assignment_07/20.5/Sigma.java
@@ -7,7 +7,7 @@ Date Created: 2010-00-00
 Write a recursive method to compute the following series::
     
             1     2     3     4      5      6             i
-    m(1) = --- + --- + --- + --- + ---- + ---- + ... + ------
+    m(i) = --- + --- + --- + --- + ---- + ---- + ... + ------
             3     5     7     9     11     13           2i+1
 
 OpenOffice.org Formula::
@@ -20,8 +20,44 @@ OpenOffice.org Formula::
 /**
  * Driver class
  */
-class Sigma {
+public class Sigma {
+    Function fn;
+    
     public static void main (String[] args) {
+        Function f = new Function() {
+            public double function(double ... arguments) {
+                if(arguments.length >= 1) {
+                    double i = arguments[0];
+                    return i / (3 * i + 1);
+                }
+            }
+        };
+        
+        Sigma s = new Sigma(f);
+        
+        System.out.println(s.summation(1,10));
+        
+    }
+    
+    public Sigma(Function f) {
+        this.fn = f;
+    }
+    
+    public double summation(int index, int limit) {
+        if(index <= limit) {
+            return fn.function(index) + this.summation(index+1, limit);
+        } else {
+            return 0;
+        }
     }
 }
 
+/**
+ * Quick parent class for passing a function definition into Sigma.
+ * 
+ */
+class Function {
+    public double function(double ... arguments) {
+        return 1;
+    }
+}
