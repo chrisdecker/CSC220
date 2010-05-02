f43d690c058ea28d184bd14290fe7273ca1842dd
Erik Youngren <artanis.00@gmail.com>
2010-05-02 00:10:54 -0700

    A7 P19.2 complete

--- Assignment_07/19.2/RandomBinary.java
+++ Assignment_07/19.2/RandomBinary.java
@@ -10,11 +10,21 @@ into the file using binary I/O.
 
 */
 
+import java.io.FileOutputStream;
+
 /**
  * Driver class
  */
 class RandomBinary {
-    public static void main (String[] args) {
+    public static void main (String[] args)
+        throws java.io.IOException
+    {
+        FileOutputStream out = new FileOutputStream("Exercise_19_2.dat");
+        for(int i = 0; i < 100; i++) {
+            int random = (int)(Math.random() * 256);
+            System.out.println(random);
+            out.write(random);
+        }
     }
 }
 
